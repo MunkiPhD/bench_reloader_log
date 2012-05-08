1ef1702b064e483664ddb55e163e706aae3a7b2d
MunkiPhD <munkiphd@gmail.com>
2012-05-08 10:29:28 -0400

    initial seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+puts "Seeding Manufacturer Data"
+
+# Manufacturer.create(name: "", abbreviation: "")
+Manufacturer.create(name: "Alaska Bullet Company", abbreviation: "AK")
+Manufacturer.create(name: "Barnes", abbreviation: "BAR")
+Manufacturer.create(name: "Berger", abbreviation: "BRGR")
+Manufacturer.create(name: "Berry's", abbreviation: "BRY")
+Manufacturer.create(name: "Cast Performance", abbreviation: "CP")
+Manufacturer.create(name: "Federal", abbreviation: "FD")
+Manufacturer.create(name: "GS Custom Bullets", abbreviation: "GS")
+Manufacturer.create(name: "Hornady", abbreviation: "HDY")
